985aba8f5d58a41a54e5470abd96f8eece860827
Cahaya Pramesta Lailatul Latifah <cahayapramestalailatullathifa@gmail.com>
2026-08-22 10:15:32 +0700

    Add files via upload

---
+++ cahaya_pramesta_lailatul_latifah.py
@@ -0,0 +1,5 @@
+print("Nama : Cahaya Pramesta Lailatul Latifah") #ini perintah untuk menampilkan Nama
+print("NIM : 2605060041") #ini perintah untuk menampilkan NIM
+print("Tempat, Tanggal Lahir : Magelang, 7 Oktober 2007") #ini perintah untuk menampilkan Tempat, Tanggal Lahir
+print("Jenis Kelamin : Perempuan") #ini perintah untuk menampilkan Jenis Kelamin
+print("Hobi : Membaca novel dan menonton drama Korea serta anime") #ini perintah untuk menampilkan Hobi
