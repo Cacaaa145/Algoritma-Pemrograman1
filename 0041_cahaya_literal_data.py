67ba16e322afd1b5dba61ab0a2e43c4c7bfd014b
Cahaya Pramesta Lailatul Latifah <cahayapramestalailatullathifa@gmail.com>
2026-08-28 20:25:30 +0700

    Add files via upload

---
+++ 0041_cahaya_literal_data.py
@@ -0,0 +1,47 @@
+print("Tugas 1 -> Deklarasi program") #mencetak teks
+print() #digunakan untuk memberikan jeda per baris
+#Tipe data : Kumpulan karakter (String)
+data_string = "Cahaya Pramesta Lailatul Latifah" #menyimpan data String
+print("Nama   : ", data_string) #mencetak data String
+#Tipe data : Angka bulat (Integer)
+data_integer = 18 #menyimpan data Integer
+print("Umur   : ", data_integer) #mencetak data Integer
+#Tipe data : Angka desimal (Float)
+data_float = 65.4 #menyimpan data Float
+print("Berat  : ", data_float) #mencetak data Float
+data_bool = True #menyimpan data Boolean
+print("Status : ", data_bool) #mencetak data Boolean
+print() #digunakan untuk memberikan jeda per baris
+print() #digunakan untuk memberikan jeda per baris
+print("Tugas 2 -> Ubah tipe data") #mencetak teks
+print() #digunakan untuk memberikan jeda per baris
+print("1. Konversi angka_string menjadi integer") #mencetak data
+angka_string = "123" #menyimpan data
+angka_integer  = int(angka_string) #menyimpan data
+print("angka = ", angka_integer) #mencetak data
+print("2. Konversi angka_float menjadi integer")  #mencetak data
+angka_float = 45.67 #menyimpan data
+angka_integer = int (angka_float) #menyimpan data
+print("angka = ", angka_integer) #mencetak data
+print("3. Konversi angka_integer menjadi float ") #mencetak data
+angka_integer = 89 #menyimpan data
+angka_float = float (angka_integer) #menyimpan data
+print("angka = ", angka_float)   #mencetak data
+print("4. Konversi angka_integer menjadi string") #mencetak data
+angka_integer = 89 #menyimpan data
+angka_string = str (angka_integer) #menyimpan data
+print("angka = ", angka_string)  #mencetak data
+print() #digunakan untuk memberikan jeda per baris
+print() #digunakan untuk memberikan jeda per baris
+print("Tugas 3 -> Program Input")
+print() #digunakan untuk memberikan jeda per baris
+
+#Meminta input nama (string)
+data = input("Masukan Nama : ") #untuk input data String
+
+#Meminta input usia (integer)
+angka = int (input("Masukan Usia : ")) #untuk input data Integer
+
+#Meminta input tinggi badan (float)
+tinggi = float (input("Masukan Tinggi Badan : ")) #untuk input data Float
+
