fcf0e19b092090a540b40dc530fcfbc1341c549c
Cahaya Pramesta Lailatul Latifah <cahayapramestalailatullathifa@gmail.com>
2026-09-04 19:45:06 +0700

    Add files via upload

---
+++ 0041_cahaya_operasi_aritmatika_dan_komperasi.py
@@ -0,0 +1,30 @@
+print("Tugas Pertemuan 3 -> Operasi Aritmatika dan Komperasi") #mencetak teks
+p = panjang = 12
+l = lebar = 5
+t = tinggi = 8
+print() #digunakan untuk memberikan jeda
+print("A. Luas, Volume, dan Keliling")
+print() #digunakan untuk memberikan jeda
+print("1. Luas") 
+hasil = p * l
+print(p,'*',l,'=',hasil)
+print() #digunakan untuk memberikan jeda 
+print("2. Volume")
+hasil = p * l * t
+print(p,'',l,'',t,'=',hasil)
+print() #digunakan untuk memberikan jeda 
+print("3. Keliling")
+hasil = 2 * (p + l)
+print('2 * (',p,'+',l,') =',hasil)
+print() #digunakan untuk memberikan jeda 
+print() #digunakan untuk memberikan jeda
+print("B. Luas bangunan lebih dari 50")
+Luas = p * l
+hasil = Luas > 50
+print(Luas,'>',50,'=',hasil) 
+print() #digunakan untuk memberikan jeda
+print("C. Volume sama dengan 480")
+Volume = p * l * t
+hasil = Volume == 480
+print(Volume,'==',480,'=',hasil) 
+print() #digunakan untuk memberikan jeda
