9727f737b2b20f5885e1bad36512b2725ee5b9c7
Cahaya Pramesta Lailatul Latifah <cahayapramestalailatullathifa@gmail.com>
2026-09-05 09:43:38 +0700

    Refactor arithmetic operations and comparisons

--- 0041_cahaya_operasi_aritmatika_dan_komperasi.py
+++ 0041_cahaya_operasi_aritmatika_dan_komperasi.py
@@ -1,30 +1,30 @@
 print("Tugas Pertemuan 3 -> Operasi Aritmatika dan Komperasi") #mencetak teks
-p = panjang = 12
-l = lebar = 5
-t = tinggi = 8
+p = panjang = 12 #Variabel p untuk panjang
+l = lebar = 5 #Variabel l untuk lebar
+t = tinggi = 8 #Variabel t untuk tinggi
 print() #digunakan untuk memberikan jeda
-print("A. Luas, Volume, dan Keliling")
+print("A. Luas, Volume, dan Keliling") #Mencetak data string
 print() #digunakan untuk memberikan jeda
-print("1. Luas") 
-hasil = p * l
-print(p,'*',l,'=',hasil)
+print("1. Luas") #Mencetak data string
+hasil = 2 * ( p * l + p * t + l * t) #Variabel hasil
+print(hasil) #Mencetak hasil
 print() #digunakan untuk memberikan jeda 
-print("2. Volume")
-hasil = p * l * t
-print(p,'',l,'',t,'=',hasil)
+print("2. Volume") #Mencetak data string
+hasil = p * l * t #Variabel hasil
+print(hasil) #Mencetak hasil
 print() #digunakan untuk memberikan jeda 
-print("3. Keliling")
-hasil = 2 * (p + l)
-print('2 * (',p,'+',l,') =',hasil)
+print("3. Keliling") #Mencetak data string
+hasil = 4 * (p + l + t) #Variabel hasil
+print(hasil) #Mencetak hasil
 print() #digunakan untuk memberikan jeda 
 print() #digunakan untuk memberikan jeda
-print("B. Luas bangunan lebih dari 50")
-Luas = p * l
-hasil = Luas > 50
-print(Luas,'>',50,'=',hasil) 
+print("B. Luas bangunan lebih dari 50") #Mencetak data string
+Luas = 2 * ( p * l + p * t + l * t) #Variabel luas
+hasil = Luas > 50 #Variabel hasil
+print(hasil) #Mencetak hasil
 print() #digunakan untuk memberikan jeda
-print("C. Volume sama dengan 480")
-Volume = p * l * t
-hasil = Volume == 480
-print(Volume,'==',480,'=',hasil) 
+print("C. Volume sama dengan 480") #Mencetak data string
+Volume = p * l * t #Variabel volume
+hasil = Volume == 480 #Variabel hasil
+print(hasil) #Mencetak hasil
 print() #digunakan untuk memberikan jeda
